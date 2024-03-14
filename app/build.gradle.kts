157723e3c5981262b57fcb18981d7d25a2f14f57
zjun <565061763@qq.com>
2024-03-15 01:30:59 +0800

    demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,6 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.lifecycle.viewmodel)
 
-    implementation(libs.sd.coroutine)
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.androidx.test.espresso.core)
--- app/src/main/java/com/sd/demo/network/SampleAwaitNetwork.kt
+++ app/src/main/java/com/sd/demo/network/SampleAwaitNetwork.kt
@@ -2,45 +2,29 @@ package com.sd.demo.network
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.sd.demo.network.databinding.SampleAwaitNetworkBinding
-import com.sd.lib.coroutine.FScope
 import com.sd.lib.network.fAwaitNetworkAvailable
+import kotlinx.coroutines.launch
 import java.util.UUID
 
 class SampleAwaitNetwork : AppCompatActivity() {
     private val _binding by lazy { SampleAwaitNetworkBinding.inflate(layoutInflater) }
 
-    private val _scope = FScope()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(_binding.root)
         _binding.btnLaunch.setOnClickListener {
-            _scope.launch {
+            lifecycleScope.launch {
                 launchNetWorkAvailable()
             }
-        }
-        _binding.btnCancel.setOnClickListener {
-            _scope.cancel()
         }
     }
 
     private suspend fun launchNetWorkAvailable() {
         val uuid = UUID.randomUUID().toString()
         logMsg { "start $uuid" }
-
-        try {
-            fAwaitNetworkAvailable()
-        } catch (e: Exception) {
-            logMsg { "exception $uuid $e" }
-            throw e
-        }
-
+        fAwaitNetworkAvailable()
         logMsg { "finish $uuid" }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _scope.cancel()
     }
 }
