51b486c5612ac69a51ba239e0f3624e00e95761f
zjun <565061763@qq.com>
2024-03-22 15:32:38 +0800

    refactor

--- lib/src/main/java/com/sd/lib/network/FNetwork.kt
+++ lib/src/main/java/com/sd/lib/network/FNetwork.kt
@@ -1,19 +1,10 @@
 package com.sd.lib.network
 
 import com.sd.lib.ctx.fContext
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
 
 object FNetwork {
-    private val _scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
     private val _networksCallback = NetworksCallback(fContext)
-
-    /** 监听所有网络 */
-    val networksFlow: Flow<List<NetworkState>>
-        get() = _networksCallback.networksFlow
 
     /** 当前网络 */
     val currentNetwork: NetworkState
@@ -23,10 +14,11 @@ object FNetwork {
     val currentNetworkFlow: Flow<NetworkState>
         get() = _networksCallback.currentNetworkFlow
 
+    /** 监听所有网络 */
+    val allNetworksFlow: Flow<List<NetworkState>>
+        get() = _networksCallback.allNetworksFlow
+
     init {
-        // 注册观察者
-        _scope.launch {
-            _networksCallback.register()
-        }
+        _networksCallback.register()
     }
 }
--- lib/src/main/java/com/sd/lib/network/NetworksCallback.kt
+++ lib/src/main/java/com/sd/lib/network/NetworksCallback.kt
@@ -5,27 +5,27 @@ import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.seconds
 
 internal class NetworksCallback(
     context: Context
 ) {
     private val _connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    private val _scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
 
     private val _networks: MutableMap<Network, NetworkState> = hashMapOf()
-    private val _networksFlow = MutableStateFlow<List<NetworkState>?>(null)
 
-    /** 监听所有网络 */
-    val networksFlow: Flow<List<NetworkState>>
-        get() = _networksFlow.filterNotNull()
+    private val _currentNetworkFlow = MutableStateFlow<NetworkState?>(null)
+    private val _allNetworksFlow = MutableStateFlow<List<NetworkState>?>(null)
 
     /** 当前网络 */
     val currentNetwork: NetworkState
@@ -33,44 +33,53 @@ internal class NetworksCallback(
 
     /** 监听当前网络 */
     val currentNetworkFlow: Flow<NetworkState>
-        get() = networksFlow
-            .map { it.filterCurrentNetwork() }
-            .filterNotNull()
-            .distinctUntilChanged()
-            .flowOn(Dispatchers.IO)
+        get() = _currentNetworkFlow.filterNotNull()
 
-    /**
-     * 注册回调对象
-     */
-    suspend fun register() {
-        val request = NetworkRequest.Builder()
-            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-            .build()
-
-        while (true) {
-            try {
-                _connectivityManager.registerNetworkCallback(request, _networkCallback)
-                break
-            } catch (e: RuntimeException) {
-                e.printStackTrace()
-                delay(1.seconds)
-            } finally {
-                updateCurrentNetwork()
-            }
-        }
-    }
+    /** 监听所有网络 */
+    val allNetworksFlow: Flow<List<NetworkState>>
+        get() = _allNetworksFlow.filterNotNull()
 
     private val _networkCallback = object : ConnectivityManager.NetworkCallback() {
         override fun onLost(network: Network) {
             super.onLost(network)
             _networks.remove(network)
-            _networksFlow.value = _networks.values.toList()
+            _allNetworksFlow.value = _networks.values.toList()
         }
 
         override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
             super.onCapabilitiesChanged(network, networkCapabilities)
             _networks[network] = network.toNetworkState(networkCapabilities)
-            _networksFlow.value = _networks.values.toList()
+            _allNetworksFlow.value = _networks.values.toList()
+        }
+    }
+
+    /**
+     * 注册回调对象
+     */
+    fun register() {
+        _scope.launch {
+            val request = NetworkRequest.Builder()
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .build()
+            while (true) {
+                try {
+                    _connectivityManager.registerNetworkCallback(request, _networkCallback)
+                    break
+                } catch (e: RuntimeException) {
+                    e.printStackTrace()
+                    delay(1.seconds)
+                } finally {
+                    updateCurrentNetwork()
+                }
+            }
+        }
+
+        _scope.launch {
+            allNetworksFlow.collectLatest { list ->
+                list.filterCurrentNetwork()?.let {
+                    _currentNetworkFlow.value = it
+                }
+            }
         }
     }
 
@@ -78,10 +87,10 @@ internal class NetworksCallback(
      * 更新当前网络状态
      */
     private fun updateCurrentNetwork() {
-        val oldList = _networksFlow.value
+        val oldList = _allNetworksFlow.value
         if (oldList.isNullOrEmpty() || oldList.size == 1) {
             val newList = listOf(_connectivityManager.networkState())
-            _networksFlow.compareAndSet(oldList, newList)
+            _allNetworksFlow.compareAndSet(oldList, newList)
         }
     }
 
