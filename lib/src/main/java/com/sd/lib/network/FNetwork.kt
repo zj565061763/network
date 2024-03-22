20f14967dd65de0cddfcd0a2685ce42d02a08676
zjun <565061763@qq.com>
2024-03-22 15:35:50 +0800

    format

--- lib/src/main/java/com/sd/lib/network/FNetwork.kt
+++ lib/src/main/java/com/sd/lib/network/FNetwork.kt
@@ -4,21 +4,21 @@ import com.sd.lib.ctx.fContext
 import kotlinx.coroutines.flow.Flow
 
 object FNetwork {
-    private val _networksCallback = NetworksCallback(fContext)
+    private val _callback = NetworksCallback(fContext)
 
     /** 当前网络 */
     val currentNetwork: NetworkState
-        get() = _networksCallback.currentNetwork
+        get() = _callback.currentNetwork
 
     /** 监听当前网络 */
     val currentNetworkFlow: Flow<NetworkState>
-        get() = _networksCallback.currentNetworkFlow
+        get() = _callback.currentNetworkFlow
 
     /** 监听所有网络 */
     val allNetworksFlow: Flow<List<NetworkState>>
-        get() = _networksCallback.allNetworksFlow
+        get() = _callback.allNetworksFlow
 
     init {
-        _networksCallback.register()
+        _callback.register()
     }
 }
