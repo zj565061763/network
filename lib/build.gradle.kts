f26a62d6cea72fa94750da5bde5eb4451d63a733
zjun <565061763@qq.com>
2024-05-01 21:19:39 +0800

    1.5.3

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "network"
-val libVersionName = "1.5.2"
+val libVersionName = "1.5.3"
 
 android {
     namespace = "com.sd.lib.network"
