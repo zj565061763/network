a0e7c7a39b57310a310a4c1b85263071860bb5e8
zjun <565061763@qq.com>
2024-10-31 21:59:17 +0800

    FNetworkObserver

--- lib/src/main/java/com/sd/lib/network/FNetwork.kt
+++ lib/src/main/java/com/sd/lib/network/FNetwork.kt
@@ -43,42 +43,6 @@ object FNetwork {
 }
 
 /**
- * 监听当前网络
- */
-abstract class FNetworkObserver {
-    private val _scope = MainScope()
-    private var _job: Job? = null
-
-    /**
-     * 注册
-     */
-    @Synchronized
-    fun register() {
-        if (_job == null) {
-            _job = _scope.launch {
-                FNetwork.currentNetworkFlow.collect {
-                    onChange(it)
-                }
-            }
-        }
-    }
-
-    /**
-     * 取消注册
-     */
-    @Synchronized
-    fun unregister() {
-        _job?.cancel()
-        _job = null
-    }
-
-    /**
-     * 当前网络状态变化(MainThread)
-     */
-    protected abstract fun onChange(networkState: NetworkState)
-}
-
-/**
  * 如果满足[condition]，直接返回，否则挂起直到满足[condition]
  */
 suspend fun fNetworkAwait(
---
+++ lib/src/main/java/com/sd/lib/network/FNetworkObserver.kt
@@ -0,0 +1,41 @@
+package com.sd.lib.network
+
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+
+/**
+ * 监听当前网络
+ */
+abstract class FNetworkObserver {
+   private val _scope = MainScope()
+   private var _job: Job? = null
+
+   /**
+    * 注册
+    */
+   @Synchronized
+   fun register() {
+      if (_job == null) {
+         _job = _scope.launch {
+            FNetwork.currentNetworkFlow.collect {
+               onChange(it)
+            }
+         }
+      }
+   }
+
+   /**
+    * 取消注册
+    */
+   @Synchronized
+   fun unregister() {
+      _job?.cancel()
+      _job = null
+   }
+
+   /**
+    * 当前网络状态变化(MainThread)
+    */
+   protected abstract fun onChange(networkState: NetworkState)
+}
